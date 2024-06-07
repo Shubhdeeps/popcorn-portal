19f1d468182f2f1ce31282984bdbf37297b0a0a3
Shubhdeeps <shubhdeep86993@gmail.com>
2024-06-07 18:46:06 +0300

    path resolve

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@
 // => high vote count
 //  => Popular in estonia
 
-import { useFetch } from "./hooks/useFetch";
-import { APIEndpoints } from "./utils/endpoints";
+import Navbar from "@/components/Layout/Navbar";
+import { useFetch } from "@/hooks/useFetch";
+import { APIEndpoints } from "@/utils/endpoints";
 
 // API reference => https://developer.themoviedb.org/reference/tv-episode-videos
 function App() {
   // const guestSessionid = "d3448f862ba6d7af29a21d77d524a899";
   // // const res1 = useFetch(APIEndpoints.GuestSession);
   // // console.log({ res1 });
-  // const res2 = useFetch(APIEndpoints.Languages);
-  // console.log({ res2 });
+  const res2 = useFetch(APIEndpoints.Languages);
+  console.log({ res2 });
 
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
