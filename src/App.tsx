c7181d7e8dc50eecac8c615702147c396becfea3
Shubhdeeps <shubhdeep86993@gmail.com>
2024-06-09 11:24:45 +0300

    init search

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@
 import { useFetch } from "@/hooks/useFetch";
 import { APIEndpoints } from "@/utils/endpoints";
 import Layout from "./components/Layout";
-import PopularMovieCard from "./features/Movie/Cards/PopularMovieCard";
-import NowPlayingMovieCard from "./features/Movie/Cards/NowPlayingMovieCard";
-import UpcomingMovieCard from "./features/Movie/Cards/UpcomingMovieCard";
-import UpcomingMovieMiniCard from "./features/Movie/Cards/UpcomingMovieMiniCard";
-import PersonCard from "./features/People/Cards/PersonCard";
-import TVCard from "./features/TV/Cards/TvCard";
 
 // API reference => https://developer.themoviedb.org/reference/tv-episode-videos
 function App() {
@@ -19,22 +13,7 @@ function App() {
 
   return (
     <Layout>
-      <div className="max-w-50">
-        <TVCard />
-      </div>
-      <PersonCard />
-      <div className="max-w-100 pt-2 pb-2">
-        <UpcomingMovieCard />
-        <div className="max-w-50">
-          <UpcomingMovieMiniCard />
-        </div>
-      </div>
-      <div className="max-w-50">
-        <NowPlayingMovieCard />
-      </div>
-      <div className="max-w-30">
-        <PopularMovieCard />
-      </div>
+      <div></div>
       {/* <div className="">
         <h2 className="">Grid System</h2>
         <div className="row border-1">
